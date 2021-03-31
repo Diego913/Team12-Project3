5f5ef2511034ee10a4f0a4eff403b60a0bb8cf64
Yuying Li <y844l178@ku.edu>
2021-03-31 17:14:21 -0500

    Basic Game

---
+++ Tetris/Blocks.py
@@ -0,0 +1,42 @@
+import random
+
+#Define some basic color and shapes(including their rotation)
+colors = [
+    (1,1,1),
+    (255, 182, 193, 255),
+    (141, 182, 205, 255),
+    (255, 160, 122, 255),
+    (240, 128, 128, 255),
+    (221, 160, 221, 255),
+    (32, 178, 170, 255)
+    ]
+blockShape = [
+        [[2,6,10,14], [0,1,2,3]],# —— and |
+        [[1,5,6,10], [2,3,5,6], [2,5,6,9], [1,2,6,7]],# Z type
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],# Left L type
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],# Right L type
+        [[2,5,6,7], [2,5,6,10], [5,6,7,10], [2,6,7,10]],# 凸 type
+        [[1, 2, 5, 6]],# suqare type
+    ]
+
+class Figure:
+    horizontal = 0
+    vertical = 0
+
+    def __init__(self, horizontal, vertical):
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.rotation=0 #record the rotation times
+        
+        self.type = random.randint(0, len(blockShape) - 1)
+        # the random color start from (255, 182, 193, 255),
+        # since the first color will disapper on the screen
+        self.color = random.randint(1, len(colors) - 1)
+    
+        #call whenever player rotate the shape
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(blockShape[self.type])
+
+    def shape(self):
+        return blockShape[self.type][self.rotation]
+    
---
+++ Tetris/main.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+from Blocks import*
+from Game import*
+   
+def print_text(screen, font, x, y, text, fcolor=(255, 255, 255)):
+    imgText = font.render(text, True, fcolor)
+    screen.blit(imgText, (x, y))
+
+pygame.init()
+screen = pygame.display.set_mode((800,700))
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
+
+
+run = True
+accelerate = 25
+speed = 0
+newGame = Game(20, 13)
+faster = False
+
+lightsteelblue = (188, 210, 238, 255)
+floralwhite= (255, 250, 240, 255)
+coral = (255, 127, 80, 255)
+font1 = pygame.font.SysFont(None, 50)  
+font2 = pygame.font.SysFont(None, 30)
+
+while run:
+    screen.fill((0,0,0))
+    pygame.draw.line(screen,coral,(450,50),(450,650),3)
+    print_text(screen, font1,490, 60, "Score:",lightsteelblue)
+    print_text(screen, font1,490,185, "Next:", lightsteelblue)
+    print_text(screen, font1,490,490, "Menu: ", lightsteelblue)
+    print_text(screen, font2,490,535, "Press Up: Rotate", floralwhite)
+    print_text(screen, font2,490,560, "Press Left: Move Left", floralwhite)
+    print_text(screen, font2,490,585, "Press Right: Move Right", floralwhite)
+    print_text(screen, font2,490,610, "Press Space: Pause", floralwhite)
+    
+    speed = speed + 1
+    # the number that speed % smaller, the downwords spees faster
+    if speed % 5 == 0 or faster:
+            newGame.DownWords()
+       
+    for i in range(0,newGame.height):
+        for j in range(0,newGame.width):
+            if newGame.field[i][j] > 0:
+                pygame.draw.rect(screen, colors[newGame.field[i][j]],[newGame.horizontal + newGame.size * j + 1, newGame.vertical + newGame.size * i + 1, newGame.size - 2, newGame.size - 1])
+            pygame.draw.rect(screen, (66, 66, 66, 255), [newGame.horizontal + newGame.size * j, newGame.vertical + newGame.size * i, newGame.size, newGame.size], 1)
+
+    if newGame.figure is None:
+        newGame.Start()   
+    else:
+        for i in range(0,newGame.height):
+            for j in range(0,4):
+                placement = i * 4 + j
+                if placement in newGame.figure.shape():
+                    pygame.draw.rect(screen, colors[newGame.figure.color],[newGame.horizontal + newGame.size * (j + newGame.figure.horizontal) + 1,newGame.vertical + newGame.size * (i + newGame.figure.vertical) + 1,newGame.size - 2, newGame.size - 2])
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                newGame.rotate()
+            if event.key == pygame.K_DOWN:
+                faster = True
+            if event.key == pygame.K_LEFT:
+                newGame.moveLeft()
+            if event.key == pygame.K_RIGHT:
+                newGame.moveRight() 
+
+        if event.type == pygame.KEYUP:
+            faster = False
+        
+        if event.type == pygame.QUIT:
+            run  = False
+
+    pygame.display.update()
+    clock.tick(accelerate)
+
+pygame.quit()
