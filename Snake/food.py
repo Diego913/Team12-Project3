3a3192d629e39b3e9952416ff26cb1459d454c03
Tucker <tucker.wienke@ku.edu>
2021-04-24 16:30:53 -0500

    Snake Game

---
+++ Snake/food.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+import random
+from settings import *
+
+
+class Food():
+    def __init__(self):
+        self.position = (0,0)
+        self.foodColor = (223, 163, 49)
+        self.randomPos()
+
+    #finds a random position on the grid to place a food square
+    def randomPos(self):
+        self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
+
+    #draws the food sprite 
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
+        pygame.draw.rect(surface, self.foodColor, r)
+        pygame.draw.rect(surface, (93, 216, 228), r, 1)
